ca09a7720fdd36acc58a71565fe7d91ec70db990
Tianlong Song <stlong0521@gmail.com>
2016-09-20 02:09:46 -0400

    Add source files to test path

--- test/test_chat_bot.py
+++ test/test_chat_bot.py
@@ -1,7 +1,4 @@
 import unittest
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../src/')
 from chat_bot import ChatBot
 
 class TestChatBot(unittest.TestCase):
