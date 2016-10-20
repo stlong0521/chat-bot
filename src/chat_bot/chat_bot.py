00b34c4cfc5cfd7c861aeb45e5a0f19f908ea104
Tianlong Song <stlong0521@gmail.com>
2016-10-20 01:16:16 -0700

    Enable safe brain memory sync

--- src/chat_bot/chat_bot.py
+++ src/chat_bot/chat_bot.py
@@ -1,16 +1,21 @@
-import os.path
+from copy import deepcopy
 from brain_graph import BrainGraph
 from sentence_trie import SentenceTrie
-from utils import load_dict_from_s3, write_dict_to_s3
+from utils import (load_dict_from_s3, write_dict_to_s3, delete_file_in_s3,
+                   load_item_from_dynamo, write_item_to_dynamo)
 
 class ChatBot:
 
     def __init__(self):
         self.mode = 'talking'
         self.graph = BrainGraph()
-        self.graph.graph = load_dict_from_s3('brain_graph.json')
         self.trie = SentenceTrie()
-        self.trie.trie = load_dict_from_s3('sentence_trie.json')
+        self.new_question_answer_pairs = []
+        self.brain_version = ('', '')
+        self.sync_memory()
+
+    def __del__(self):
+        while not self.sync_memory(): pass
 
     def interact(self):
         while True:
@@ -25,12 +30,14 @@ class ChatBot:
     def respond(self, sentence):
         if self.mode == 'learning' and self.waiting_for_answer:
             self.waiting_for_answer = False
+            self.new_question_answer_pairs.append((self.most_recent_question, sentence))
             self.learn(self.most_recent_question, sentence)
             response = 'Thanks! New information learned!'
-        elif sentence.lower() == 'bye' or sentence.lower() == 'goodbye':
-            write_dict_to_s3('brain_graph.json', self.graph.graph)
-            write_dict_to_s3('sentence_trie.json', self.trie.trie)
-            response = 'Bye!'
+        elif sentence.lower() == 'sync memory':
+            if self.sync_memory():
+                response = 'Memory synced!'
+            else:
+                response = 'Someone else is syncing! You may want to try again later!'
         elif sentence.lower() == 'switch to learning':
             self.mode = 'learning'
             self.waiting_for_answer = False
@@ -51,3 +58,38 @@ class ChatBot:
     def learn(self, question, answer):
         self.graph.process_question_answer_pair(question, answer)
         self.trie.add_sentence_to_trie(answer)
+
+    def sync_memory(self):
+        # Back up the current brain in case sync memory fails
+        brain_graph = deepcopy(self.graph.graph)
+        sentence_trie = deepcopy(self.trie.trie)
+        # If the remote brain has a diverged version, FETCH
+        latest_brain_version = load_item_from_dynamo()
+        if latest_brain_version != self.brain_version:
+            self.graph.graph = load_dict_from_s3('brain_graph.json', version=latest_brain_version[0])
+            self.trie.trie = load_dict_from_s3('sentence_trie.json', version=latest_brain_version[1])
+        # If the bot does not have any new knowledge, no need for MERGE or PUSH
+        if not self.new_question_answer_pairs:
+            self.brain_version = latest_brain_version
+            return True
+        # If the remote brain has a diverged version, MERGE
+        if latest_brain_version != self.brain_version:
+            for question, answer in self.new_question_answer_pairs:
+                self.learn(question, answer)
+        # Then, PUSH
+        updated_brain_version = (write_dict_to_s3('brain_graph.json', self.graph.graph),
+                                 write_dict_to_s3('sentence_trie.json', self.trie.trie))
+        if not write_item_to_dynamo(updated_brain_version, latest_brain_version):
+            # PUSH failed, recover the backup brain and delete the brain files just uploaded
+            self.graph.graph = brain_graph
+            self.trie.trie = sentence_trie
+            delete_file_in_s3('brain_graph.json', updated_brain_version[0])
+            delete_file_in_s3('sentence_trie.json', updated_brain_version[1])
+            return False
+        else:
+            # PUSH succeeded, update the brain version and delete the out-of-date brain files
+            self.brain_version = updated_brain_version
+            self.new_question_answer_pairs = []
+            delete_file_in_s3('brain_graph.json', latest_brain_version[0])
+            delete_file_in_s3('sentence_trie.json', latest_brain_version[1])
+            return True
--- src/chat_bot/utils.py
+++ src/chat_bot/utils.py
@@ -5,6 +5,7 @@ import simplejson as json
 import boto3, botocore
 import os
 import requests
+from datetime import datetime
 
 def tokenize(sentence):
     return word_tokenize(sentence)
@@ -24,11 +25,13 @@ def write_dict_to_file(json_file_path, dictionary):
     with open(json_file_path, "w") as json_file:
         json.dump(dictionary, json_file)
 
-def load_dict_from_s3(file_name):
+def load_dict_from_s3(file_name, version=None):
     s3_client = boto3.client('s3')
+    params={'Bucket': 'ts-chat-bot', 'Key': 'brains/' + file_name}
+    if version:
+        params['VersionId'] = version
     url = s3_client.generate_presigned_url('get_object', 
-                                           Params={'Bucket': 'ts-chat-bot',
-                                                   'Key': 'brains/' + file_name}, 
+                                           Params=params, 
                                            ExpiresIn=30)
     return json.loads(requests.get(url).text)
 
@@ -38,4 +41,34 @@ def write_dict_to_s3(file_name, dictionary):
                                            Params={'Bucket': 'ts-chat-bot',
                                                    'Key': 'brains/' + file_name}, 
                                            ExpiresIn=30)
-    requests.put(url, data=json.dumps(dictionary))
+    response = requests.put(url, data=json.dumps(dictionary))
+    return response.headers['x-amz-version-id']
+
+def delete_file_in_s3(file_name, version):
+    s3_client = boto3.client('s3')
+    s3_client.delete_object(Bucket='ts-chat-bot',
+                            Key='brains/' + file_name,
+                            VersionId=version)
+
+def load_item_from_dynamo():
+    dynamo_client = boto3.client('dynamodb')
+    response = dynamo_client.get_item(TableName='brain-version',
+                                      Key={'Tag': {'S': 'latest'}},
+                                      ProjectionExpression='BrainGraphVersion, SentenceTrieVersion')
+    return response['Item']['BrainGraphVersion']['S'], response['Item']['SentenceTrieVersion']['S']
+
+def write_item_to_dynamo(updated_brain_version, old_brain_version):
+    dynamo_client = boto3.client('dynamodb')
+    item = {'Tag': {'S': 'latest'},
+            'BrainGraphVersion': {'S': updated_brain_version[0]},
+            'SentenceTrieVersion': {'S': updated_brain_version[1]},
+            'UpdatedTime': {'S': str(datetime.now())}
+           }
+    condition_expression = 'BrainGraphVersion = :bgv and SentenceTrieVersion = :stv'
+    expression_attribute_values = {':bgv': {'S': old_brain_version[0]},
+                                   ':stv': {'S': old_brain_version[1]}}
+    response = dynamo_client.put_item(TableName='brain-version',
+                                      Item=item,
+                                      ConditionExpression=condition_expression,
+                                      ExpressionAttributeValues=expression_attribute_values)
+    return response['ResponseMetadata']['HTTPStatusCode'] == 200
